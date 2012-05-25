5302c683d3e16b1d73c4dc1fa9d49c35202949d5
bibulle <famille.martin@gmail.com>
2012-05-25 14:51:10 +0200

    suppress an unused import warning

--- Fan/src/main/java/com/em/fan/FanBlock.java
+++ Fan/src/main/java/com/em/fan/FanBlock.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Chunk;
-import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
